917fb858a02dc1445f25fe958873cb843e324a9d
Simon Mandlik <simon.mandlik@gmail.com>
2020-03-23 20:45:47 +0100

    v 0.0.5: better aligned printing

--- src/printing.jl
+++ src/printing.jl
@@ -12,8 +12,9 @@ printkeys(ch::NamedTuple) = ["$k: " for k in keys(ch)]
 
 function _printtree(io::IO, n, C, d, p, e, trav, trunc_level)
     c = isa(NodeType(n), LeafNode) ? :white : C[1+d%length(C)]
-    gap = "g" ^ min(2, length(noderepr(n))-1)
-    paddedprint(io, noderepr(n) * (trav ? ' ' * "[\"$(stringify(e))\"]" : ""), color=c)
+    nr = noderepr(n)
+    gap = " " ^ min(2, length(nr)-1)
+    paddedprint(io, nr * (trav ? ' ' * "[\"$(stringify(e))\"]" : ""), color=c)
     CH = printchildren_sorted(n)
     nch = length(CH)
     if nch > 0
@@ -27,7 +28,7 @@ function _printtree(io::IO, n, C, d, p, e, trav, trunc_level)
                 println(io)
                 line = (i == nch ? "└" : "├") * "─" ^ (2 + l-length(pk))
                 paddedprint(io, gap * line * " " * pk, color=c, pad=p)
-                ns = gap * (i == nch ? ' ' : '│') * "x" ^ (3+l)
+                ns = gap * (i == nch ? ' ' : '│') * " " ^ (3+l)
                 _printtree(io, ch, C, d+1, [p; (c, ns)], e * encode(i, nch), trav, trunc_level)
             end
         end
