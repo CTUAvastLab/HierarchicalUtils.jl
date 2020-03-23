c4cc06e74d585350c913212877bcb3c6d9a8d521
Simon Mandlik <simon.mandlik@gmail.com>
2020-03-23 20:37:59 +0100

    v 0.0.5: better aligned printing

--- src/printing.jl
+++ src/printing.jl
@@ -12,23 +12,22 @@ printkeys(ch::NamedTuple) = ["$k: " for k in keys(ch)]
 
 function _printtree(io::IO, n, C, d, p, e, trav, trunc_level)
     c = isa(NodeType(n), LeafNode) ? :white : C[1+d%length(C)]
-    gap = " " ^ min(2, length(noderepr(n))-1)
+    gap = "g" ^ min(2, length(noderepr(n))-1)
     paddedprint(io, noderepr(n) * (trav ? ' ' * "[\"$(stringify(e))\"]" : ""), color=c)
     CH = printchildren_sorted(n)
-    PK = printkeys(CH)
-    l = max(length.(PK))
     nch = length(CH)
-    @show "ahoj"
     if nch > 0
         if d >= trunc_level
             println(io)
             paddedprint(io, gap * '⋮', color=c, pad=p)
         else
+            PK = printkeys(CH)
+            l = maximum(length.(PK))
             for (i, (ch, pk)) in enumerate(zip(CH, PK))
                 println(io)
                 line = (i == nch ? "└" : "├") * "─" ^ (2 + l-length(pk))
                 paddedprint(io, gap * line * " " * pk, color=c, pad=p)
-                ns = gap * (i == nch ? ' ' : '│') * repeat(" ", max(3, 2+l))
+                ns = gap * (i == nch ? ' ' : '│') * "x" ^ (3+l)
                 _printtree(io, ch, C, d+1, [p; (c, ns)], e * encode(i, nch), trav, trunc_level)
             end
         end
