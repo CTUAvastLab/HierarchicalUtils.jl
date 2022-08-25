9b7883deb431e4429170cd8fca360efafcf3f922
Simon Mandlik <simon.mandlik@gmail.com>
2022-08-25 10:26:33 +0200

    added limited printing

--- src/printing.jl
+++ src/printing.jl
@@ -1,46 +1,109 @@
 const COLORS = [:blue, :red, :green, :yellow, :cyan, :magenta]
-function paddedprint(io, s...; color=:default, pad=[])
+const H_LINE = '─'
+const H_ELLIPSIS = '⋯'
+const V_LINE = '│'
+const V_ELLIPSIS = '┊'
+const SPLIT = '├'
+const ENDING = '╰'
+
+mutable struct DisplaySizePrinter{T <: IO, U <: Number}
+    io::T
+    W::U
+    w::U
+    h::U
+end
+
+DisplaySizePrinter(io::IO, W::Number, H::Number) = DisplaySizePrinter(io, W, W, H)
+
+function Base.println(p::DisplaySizePrinter, pad)
+    p.h > 0 || return
+    if p.h ≥ 1
+        println(p.io)
+        p.w = p.W
+    end
+    if p.h == 1
+        print_last(p, pad)
+    end
+    p.h -= 1
+end
+
+function printstyled(p::DisplaySizePrinter, s; kwargs...)
+    (p.w > 0 && p.h > 0) || return
+    if p.w ≤ 1 + length(s)
+        s = chop(s, tail=length(s) - p.w + 1)
+        if !endswith(s, ' ')
+            s = chop(s, tail=1) * ' '
+        end
+        Base.printstyled(p.io, s; kwargs...)
+        Base.printstyled(p.io, repeat(' ', p.w - length(s) - 1) * H_ELLIPSIS; color=:light_black)
+        p.w = 0
+    else
+        p.w -= length(s)
+        Base.printstyled(p.io, s; kwargs...)
+    end
+end
+
+function print_last(printer::DisplaySizePrinter, pad; kwargs...)
+    printer.h -= 1
+    for (c, p) in pad
+        Base.printstyled(printer.io, replace(p, V_LINE => V_ELLIPSIS); color=c)
+    end
+end
+
+function paddedprint(io, s; pad=[], color=:default, kwargs...)
     @nospecialize
     for (c, p) in pad
-        printstyled(io, p, color=c)
+        printstyled(io, p; color=c, kwargs...)
     end
-    printstyled(io, s..., color=color)
+    printstyled(io, s; color, kwargs...)
 end
 
 _printkeys(ch) = ["" for _ in eachindex(ch)]
 _printkeys(ch::Union{NamedTuple, Dict, OrderedDict}) = ["$k: " for k in keys(ch)]
-_printkeys(ch::PairVec) = ["$k: " for (k,v) in ch]
+_printkeys(ch::PairVec) = ["$k: " for (k, _) in ch]
 
-function _printtree(io::IO, n, C, d, p, e, trav, htrunc, vtrunc, comments)
-    @nospecialize
-    c = isa(NodeType(n), LeafNode) ? :default : C[1+d%length(C)]
-    nr = sprint(nodeshow, n, context=io)
-    paddedprint(io, nr * (trav ? ' ' * "[\"$(stringify(e))\"]" : ""), color=c)
-    if comments
-        nc = sprint(nodecommshow, n, context=io)
-        paddedprint(io, isempty(nc) ? "" : " ", nc, color=:light_black)
+function _print_current(printer, n, c, e, trav, comments)
+    nr = sprint(nodeshow, n, context=printer.io)
+    if trav
+        nr *= " [\"" * stringify(e) * "\"]"
     end
+    paddedprint(printer, nr, color=c)
+    if comments
+        nc = sprint(nodecommshow, n, context=printer.io)
+        if !isempty(nc)
+            paddedprint(printer, ' ' * nc, color=:light_black)
+        end
+    end
+    gap = repeat(' ', clamp(length(nr)-1, 0, 2))
+    return gap
+end
+
+function _printtree(printer, n, C, d, p, pl, e, trav, htrunc, vtrunc, comments)
+    @nospecialize
+    printer.h > 0 || return
+
+    c = isa(NodeType(n), LeafNode) ? :default : C[1 + d % length(C)]
+    gap = _print_current(printer, n, c, e, trav, comments)
 
     CH = printchildren(n)
     PK = _printkeys(CH)
     CH = _iter(CH)
     nch = length(CH)
 
-    gap = " " ^ clamp(length(nr)-1, 0, 2)
-
     function _printchild(i, ch, pk, l)
-        println(io)
-        line = (i == nch ? '└' : '├') * '─' ^ (2 + l-length(pk))
-        paddedprint(io, gap * line * ' ' * pk, color=c, pad=p)
-        ns = gap * (i == nch ? ' ' : '│') * ' ' ^ (3+l)
-        _printtree(io, ch, C, d+1, [p; (c, ns)], e * encode(i, nch), trav, htrunc, vtrunc, comments)
+        println(printer, [p; (c, gap * V_LINE)])
+        line = (i == nch ? ENDING : SPLIT) * repeat(H_LINE, 2 + l - length(pk))
+        paddedprint(printer, gap * line * ' ' * pk, color=c, pad=p)
+        ns = gap * (i == nch ? ' ' : V_LINE) * repeat(' ', 3 + l)
+        _printtree(printer, ch, C, d+1, [p; (c, ns)], pl + length(ns),
+                    e * encode(i, nch), trav, htrunc, vtrunc, comments)
     end
 
     if nch > 0
-        if d+1 >= htrunc
-            println(io)
-            paddedprint(io, gap * '⋮', color=c, pad=p)
-        elseif nch > vtrunc
+        if d+1 >= htrunc || pl + length(gap) + 6 > printer.W
+            println(printer, [p; (c, gap * V_LINE)])
+            paddedprint(printer, gap * V_ELLIPSIS, color=c, pad=p)
+        elseif nch > vtrunc && vtrunc + 1 < printer.h
             l2 = vtrunc < 2 ? 0 : (vtrunc < 4 ? 1 : 2)
             l1 = vtrunc - l2
             int1, int2 = 1:l1, nch-l2+1:nch
@@ -51,8 +114,8 @@ function _printtree(io::IO, n, C, d, p, e, trav, htrunc, vtrunc, comments)
                 _printchild(i, ch, pk, l)
             end
 
-            println(io)
-            paddedprint(io, gap * '⋮' , color=c, pad=p)
+            println(printer, [p; (c, gap * V_LINE)])
+            paddedprint(printer, gap * V_ELLIPSIS , color=c, pad=p)
 
             for (i, (ch, pk)) in coll[int2]
                 _printchild(i, ch, pk, l)
@@ -67,11 +130,20 @@ function _printtree(io::IO, n, C, d, p, e, trav, htrunc, vtrunc, comments)
 end
 
 printtree(n; kwargs...) = printtree(stdout, n; kwargs...)
-function printtree(io::IO, n; trav::Bool=false, htrunc::Real=Inf, vtrunc::Real=Inf,
-                        breakline::Bool=true, comments::Bool=true)
+function printtree(io::IO, n; trav::Bool=false, htrunc::Number=Inf, vtrunc::Number=Inf,
+                    limit::Bool=get(io, :limit, true), breakline::Bool=true, comments::Bool=true)
     @nospecialize
     @assert htrunc ≥ 0 "htrunc must be ≥ 0"
     @assert vtrunc ≥ 0 "vtrunc must be ≥ 0"
-    _printtree(io, n, COLORS, 0, [], "", trav, htrunc, vtrunc, comments)
-    breakline && println(io)
+    H, W = limit ? displaysize(io) : (Inf, Inf)
+    # 3 for "julia>" prompts + 1 for breakline
+    H -= 3 + breakline
+    # print at least one node + ellipsis
+    H = max(H, 2)
+    # print at least three characters before ellipsis
+    W = max(W, 5)
+    _printtree(DisplaySizePrinter(io, W, H), n, COLORS, 0, [], 0, "", trav, htrunc, vtrunc, comments)
+    if breakline
+        println(io)
+    end
 end
--- test/printing.jl
+++ test/printing.jl
@@ -1,12 +1,15 @@
-function numlines(buf)
-    s = String(take!(buf))
+function context_sprintf(f, args...; context=IOContext(IOBuffer()), kwargs...)
+    f(context, args...; kwargs...)
+    String(take!(context.io))
+end
+
+function numlines(f, args...; kwargs...)
+    s = context_sprintf(f, args...; kwargs...)
     sum(!isempty(strip(c)) for c in split(s, '\n'))
 end
 
 @testset "printtree length" for t in TEST_TREES
-    buf = IOBuffer()
-    printtree(buf, t)
-    @test numlines(buf) == nnodes(t)
+    @test numlines(printtree, t) == nnodes(t)
 end
 
 @testset "printtree horizontal truncation for infinite trees" begin
@@ -14,23 +17,85 @@ end
     push!(t1.chs, t1)
     t2 = Dict()
     t2["ch"] = t2
-    buf1 = IOBuffer()
-    printtree(buf1, t1; htrunc=10)
-    buf2 = IOBuffer()
-    printtree(buf2, t2; htrunc=20)
-    @test numlines(buf1) == 10 + 1 # one line for ellipsis ⋮
-    @test numlines(buf2) == 20 + 1 # one line for ellipsis ⋮
+    @test numlines(printtree, t1, htrunc=10, limit=false) == 10 + 1 # one line for ellipsis
+    @test numlines(printtree, t2, htrunc=20, limit=false) == 20 + 1 # one line for ellipsis
 end
 
 @testset "printtree vertical truncation for many children" for l in 0:20, vtrunc in 0:20
     v = 1:l |> collect
-    buf = IOBuffer()
-    printtree(buf, v; vtrunc=vtrunc)
-    @test numlines(buf) == min(l, vtrunc + 1) + 1 # one line for ellipsis ⋮ and one for the header
+    @test numlines(printtree, v, vtrunc=vtrunc, limit=false) == min(l, vtrunc + 1) + 1 # one line for ellipsis and one for the header
+end
+
+@testset "printtree limit" begin
+    t = NTVertex(0, (a=Leaf(1), b=Leaf(2), c=Leaf(3)))
+    displaysize = (1, 1)
+    context = IOContext(IOBuffer(), :limit => true, :displaysize => displaysize)
+    @test context_sprintf(printtree, t; context) ==
+        """
+        NTV ⋯
+          ┊
+        """
+    displaysize = (10, 1)
+    context = IOContext(IOBuffer(), :limit => true, :displaysize => displaysize)
+    @test context_sprintf(printtree, t; context) ==
+        """
+        NTV ⋯
+          ┊
+        """
+    displaysize = (1, 7)
+    context = IOContext(IOBuffer(), :limit => true, :displaysize => displaysize)
+    @test context_sprintf(printtree, t; context) ==
+        """
+        NTVer ⋯
+          ┊
+        """
+    displaysize = (6, 8)
+    context = IOContext(IOBuffer(), :limit => true, :displaysize => displaysize)
+    @test context_sprintf(printtree, t; context) ==
+        """
+        NTVert ⋯
+          ├──  ⋯
+          ┊
+        """
+    displaysize = (7, 8)
+    context = IOContext(IOBuffer(), :limit => true, :displaysize => displaysize)
+    @test context_sprintf(printtree, t; context) ==
+        """
+        NTVert ⋯
+          ├──  ⋯
+          ├──  ⋯
+          ┊
+        """
+    displaysize = (8, 8)
+    context = IOContext(IOBuffer(), :limit => true, :displaysize => displaysize)
+    @test context_sprintf(printtree, t; context) ==
+        """
+        NTVert ⋯
+          ├──  ⋯
+          ├──  ⋯
+          ╰──  ⋯
+        """
+    displaysize = (6, 10)
+    context = IOContext(IOBuffer(), :limit => true, :displaysize => displaysize)
+    @test context_sprintf(printtree, t; context) ==
+        """
+        NTVertex ⋯
+          ├── a: ⋯
+          ┊
+        """
+    displaysize = (7, 8)
+    context = IOContext(IOBuffer(), :limit => true, :displaysize => displaysize)
+    @test context_sprintf(printtree, t; context, vtrunc=2) ==
+        """
+        NTVert ⋯
+          ├──  ⋯
+          ├──  ⋯
+          ┊
+        """
 end
 
 @testset "printtree labelled children not sorted" begin
-    t = NTVertex(0, (; a=Leaf(1), b=Leaf(2), c=Leaf(3)))
+    t = NTVertex(0, (a=Leaf(1), b=Leaf(2), c=Leaf(3)))
     buff = IOBuffer()
     printtree(buff, t)
     @test String(take!(buff)) ==
@@ -38,10 +103,10 @@ end
         NTVertex (0) comm
           ├── a: Leaf (1) comm
           ├── b: Leaf (2) comm
-          └── c: Leaf (3) comm
+          ╰── c: Leaf (3) comm
         """
 
-    t = NTVertex(0, (; c=Leaf(3), b=Leaf(2), a=Leaf(1)))
+    t = NTVertex(0, (c=Leaf(3), b=Leaf(2), a=Leaf(1)))
     buff = IOBuffer()
     printtree(buff, t)
     @test String(take!(buff)) ==
@@ -49,10 +114,10 @@ end
         NTVertex (0) comm
           ├── c: Leaf (3) comm
           ├── b: Leaf (2) comm
-          └── a: Leaf (1) comm
+          ╰── a: Leaf (1) comm
         """
 
-    t = NTVertex(0, (; b=Leaf(2), c=Leaf(3), a=Leaf(1)))
+    t = NTVertex(0, (b=Leaf(2), c=Leaf(3), a=Leaf(1)))
     buff = IOBuffer()
     printtree(buff, t)
     @test String(take!(buff)) ==
@@ -60,7 +125,7 @@ end
         NTVertex (0) comm
           ├── b: Leaf (2) comm
           ├── c: Leaf (3) comm
-          └── a: Leaf (1) comm
+          ╰── a: Leaf (1) comm
         """
 end
 
@@ -73,10 +138,10 @@ end
         BinaryVertex (1) comm
           ├── BinaryVertex (2) comm
           │     ├── Leaf (4) comm
-          │     └── Leaf (5) comm
-          └── BinaryVertex (3) comm
+          │     ╰── Leaf (5) comm
+          ╰── BinaryVertex (3) comm
                 ├── Leaf (6) comm
-                └── Leaf (7) comm
+                ╰── Leaf (7) comm
         """
     buf = IOBuffer()
     printtree(buf, t; trav=true)
@@ -85,9 +150,9 @@ end
         BinaryVertex (1) [""] comm
           ├── BinaryVertex (2) ["E"] comm
           │     ├── Leaf (4) ["I"] comm
-          │     └── Leaf (5) ["M"] comm
-          └── BinaryVertex (3) ["U"] comm
+          │     ╰── Leaf (5) ["M"] comm
+          ╰── BinaryVertex (3) ["U"] comm
                 ├── Leaf (6) ["Y"] comm
-                └── Leaf (7) ["c"] comm
+                ╰── Leaf (7) ["c"] comm
         """
 end
